5867e1dc0e4a3320d90a198b80a302aa99ddecb3
nicolasvintrob <87212117+nicolasvintrob@users.noreply.github.com>
2021-11-28 20:50:55 -0300

    Add files via upload

---
+++ Clase 8/Clase8.js
@@ -0,0 +1,22 @@
+function validar(){
+    let item = document.getElementById("nombreItem");
+    let precio = document.getElementById("precioProducto");
+    let cuotas = document.getElementById("cantidadCuotas");
+    let precioTotal = (precio/cuotas);
+
+    let parrafo = document.createElement("p");
+    parrafo.innerHTML = ("El precio de la cuota por: " + item + "Es de: " + cuotas);
+    mensaje.appendChild(parrafo);
+
+    console.log(precio.value);
+    console.log(cuotas.value);
+    console.log(precioTotal.value);
+    console.log(item.value);
+}
+
+
+
+
+
+alert("Ingrese los datos del producto para calcular las cuotas");
+
